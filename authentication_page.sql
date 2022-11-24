8280b7824e735fdb1f303d69bd1fb102e67f3a14
Dayawant1 <93541277+Dayawant1@users.noreply.github.com>
2022-11-24 23:09:32 +0530

    Add files via upload

---
+++ authentication_page.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 24, 2022 at 04:20 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `authentication_page`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_member`
+--
+
+CREATE TABLE `tbl_member` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_member`
+--
+
+INSERT INTO `tbl_member` (`id`, `username`, `password`, `email`, `create_at`) VALUES
+(7, 'ram', '$2y$10$IM9.QohluemNwQzcxseZ/eNKb7mu5uM/fX.U1tF1Fdy2IMXZxxqhS', 'ram@gmail.com', '2022-11-24 14:59:01'),
+(8, 'shyam', '$2y$10$8fjx9OTyrTUtua6p65r00.74nKtGLbzjxvpY01qfj8UVnZXaSN2A.', 'shyam@gmail.com', '2022-11-24 15:00:43'),
+(9, '', '$2y$10$YD04WcL8w1qIlOHKlTv/feTrz/zjMv8YTgTe9iD0pjpnX1621.3SG', '', '2022-11-24 15:18:53');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_member`
+--
+ALTER TABLE `tbl_member`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_member`
+--
+ALTER TABLE `tbl_member`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
